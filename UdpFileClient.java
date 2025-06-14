d63dd0e7282e19fb6d7712eb202b888a4a30dbe1
xiaodai-20233006436 <3471987073@qq.com>
2025-06-14 14:11:25 +0800

    Initialization of core variables and clients
    There are core variables here, as well as initialization code for the client, including the creation of UDP sockets

--- UdpFileClient.java
+++ UdpFileClient.java
@@ -4,5 +4,15 @@ import java.util.Scanner;
 import java.nio.charset.StandardCharsets;
 
 public class UdpFileClient
-{
+{   //Core member variables
+    private DatagramSocket socket;
+    private String serverIp;
+    private int serverPort;
+    private final int bufferSize = 4096;
+    public UdpFileClient(String serverIp, int serverPort) throws SocketException//Initialize the client
+    {
+        this.socket = new DatagramSocket();//Create a UDP socket
+        this.serverIp = serverIp;
+        this.serverPort = serverPort;
+    }
 }
